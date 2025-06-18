daa94028aad8ce0961d698f2ad823636d5536d81
Priyasha Sabbavarapu <priyashadevi@gmail.com>
2025-06-18 14:24:21 +0530

    Create two_sum_leetcode.java

---
+++ two_sum_leetcode.java
@@ -0,0 +1,21 @@
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        Map <Integer,Integer> map = new HashMap<>();
+        int [] result = new int[2];
+        for(int i=0;i<n;i++)
+        {
+            if(map.containsKey(target-nums[i]))
+            {
+                result[1]=i;
+                result[0]=map.get(target-nums[i]);
+                return result;
+            }
+            map.put(nums[i],i);
+        }
+        return result;
+    }
+}
+//rather than taking the whole map =...jus take the map after checking target-nums[i]
